8d7503d69fd2a273b668873165fa7ee0b6efdefd
Xavier <xavier.etchevers@orange.com>
2023-03-15 14:40:11 +0100

    On a wide enough edge, computed reluctance with walk mode remains unchanged

--- src/test/java/org/opentripplanner/routing/edgetype/StreetEdgeReluctanceCalculatorTest.java
+++ src/test/java/org/opentripplanner/routing/edgetype/StreetEdgeReluctanceCalculatorTest.java
@@ -3,6 +3,7 @@ package org.opentripplanner.routing.edgetype;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.OptionalDouble;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.routing.api.request.preference.RoutingPreferences;
 import org.opentripplanner.routing.api.request.preference.WalkPreferences;
@@ -10,18 +11,35 @@ import org.opentripplanner.routing.core.TraverseMode;
 
 class StreetEdgeReluctanceCalculatorTest {
 
-  private double doubleAccuracy = 0.0000000001;
+  private double defaultWalkReluctance;
+
+  @BeforeEach
+  void setup() {
+    defaultWalkReluctance =
+      StreetEdgeReluctanceCalculator.computeReluctance(
+        new RoutingPreferences(),
+        TraverseMode.WALK,
+        false,
+        false,
+        OptionalDouble.empty()
+      );
+  }
 
   @Test
   void computedReluctanceWithWalkModeIsDefaultWalkReluctance() {
-    double defaultWalkReluctance = StreetEdgeReluctanceCalculator.computeReluctance(
+    assertEquals(WalkPreferences.DEFAULT.reluctance(), defaultWalkReluctance);
+  }
+
+  @Test
+  void onAWideEnoughEdgeComputedReluctanceWithWalkModeDoesNotIncrease() {
+    double walkReluctance = StreetEdgeReluctanceCalculator.computeReluctance(
       new RoutingPreferences(),
       TraverseMode.WALK,
       false,
       false,
-      OptionalDouble.empty()
+      OptionalDouble.of(1.0)
     );
 
-    assertEquals(WalkPreferences.DEFAULT.reluctance(), defaultWalkReluctance, doubleAccuracy);
+    assertEquals(defaultWalkReluctance, walkReluctance);
   }
 }
