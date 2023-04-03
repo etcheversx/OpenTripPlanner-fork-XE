899afc130b08604a58d802f42b77805b3981a440
Xavier <xavier.etchevers@orange.com>
2023-04-03 11:26:33 +0200

    REFACTO: parametrizing and adding unit test on walk reluctance computing

--- src/test/java/org/opentripplanner/routing/edgetype/StreetEdgeReluctanceCalculatorTest.java
+++ src/test/java/org/opentripplanner/routing/edgetype/StreetEdgeReluctanceCalculatorTest.java
@@ -3,6 +3,7 @@ package org.opentripplanner.routing.edgetype;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.OptionalDouble;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -12,6 +13,15 @@ import org.opentripplanner.routing.api.request.preference.WalkPreferences;
 import org.opentripplanner.routing.core.TraverseMode;
 
 class StreetEdgeReluctanceCalculatorTest {
+  private RoutingPreferences.Builder routingPreferencesBuilder;
+
+  @BeforeEach
+  void setup() {
+    routingPreferencesBuilder = new RoutingPreferences.Builder(
+      new RoutingPreferences()
+    );
+  }
+
   @Test
   void computedReluctanceWithWalkModeIsDefaultWalkReluctance() {
     assertEquals(2.0, WalkPreferences.DEFAULT.reluctance());
@@ -26,52 +36,43 @@ class StreetEdgeReluctanceCalculatorTest {
     "0.9, 0.85, 4.0"
   })
   void testReluctanceProcessingWithWidth(Double minimalWidth, Double edgeWidth, Double expectedWalkReluctance) {
-    RoutingPreferences.Builder routingPreferencesBuilder = new RoutingPreferences.Builder(
-      new RoutingPreferences()
-    );
     if (minimalWidth != null) {
       routingPreferencesBuilder.withWalk(w -> w.withMinimalWidth(minimalWidth));
     }
-    RoutingPreferences routingPreferences = routingPreferencesBuilder.build();
 
-    double walkReluctance = StreetEdgeReluctanceCalculator.computeReluctance(
-      routingPreferences,
-      TraverseMode.WALK,
-      false,
-      false,
-      edgeWidth != null ? OptionalDouble.of(edgeWidth) : OptionalDouble.empty(),
-      OptionalBoolean.empty());
-
-    assertEquals(expectedWalkReluctance, walkReluctance);
+    assertEquals(expectedWalkReluctance, computeWalkReluctance(edgeWidth, null));
   }
 
   @ParameterizedTest(name = "Walk reluctance with requiredLight={0} on edge with light={1} is {2}")
   @CsvSource({
+    ", , 2.0",
+    "true, , 2.0",
+    "false, , 2.0",
+    ", true, 2.0",
+    ", false, 2.0",
     "true, true, 2.0",
     "false, true, 2.0",
     "true, false, 4.0",
     "false, false, 2.0"
   })
   void testReluctanceProcessingWithLight(Boolean lightRequired, Boolean edgeLight, Double expectedWalkReluctance) {
-    RoutingPreferences routingPreferences = routingPreferencesWithLit(lightRequired);
+    if (lightRequired != null) {
+      routingPreferencesBuilder.withWalk(w -> w.withLightRequired(lightRequired));
+    }
 
-    double walkReluctance = StreetEdgeReluctanceCalculator.computeReluctance(
-      routingPreferences,
+    assertEquals(expectedWalkReluctance, computeWalkReluctance(null, edgeLight));
+  }
+
+  private double computeWalkReluctance(
+    Double edgeWidth,
+    Boolean edgeLight
+  ) {
+    return StreetEdgeReluctanceCalculator.computeReluctance(
+      routingPreferencesBuilder.build(),
       TraverseMode.WALK,
       false,
       false,
-      OptionalDouble.empty(),
-      OptionalBoolean.of(edgeLight)
-    );
-
-    assertEquals(expectedWalkReluctance, walkReluctance);
-  }
-
-  private static RoutingPreferences routingPreferencesWithLit(boolean lightRequired) {
-    RoutingPreferences.Builder routingPreferencesBuilder = new RoutingPreferences.Builder(
-      new RoutingPreferences()
-    );
-    routingPreferencesBuilder.withWalk(w -> w.withLightRequired(lightRequired));
-    return routingPreferencesBuilder.build();
+      edgeWidth != null ? OptionalDouble.of(edgeWidth) : OptionalDouble.empty(),
+      edgeLight != null ? OptionalBoolean.of(edgeLight) : OptionalBoolean.empty());
   }
 }
