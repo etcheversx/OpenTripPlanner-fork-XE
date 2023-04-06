67f6290f6b3e617eada0d02e8770fb4afb620683
Xavier <xavier.etchevers@orange.com>
2023-04-06 11:11:47 +0200

    Test surface getting and setting

--- src/test/java/org/opentripplanner/graph_builder/module/osm/AccessibilityPropertySetTest.java
+++ src/test/java/org/opentripplanner/graph_builder/module/osm/AccessibilityPropertySetTest.java
@@ -4,25 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.OptionalDouble;
 import org.junit.jupiter.api.Test;
+import org.opentripplanner.openstreetmap.model.OSMSurface;
 import org.opentripplanner.openstreetmap.model.OptionalBoolean;
 import org.opentripplanner.openstreetmap.model.OptionalEnum;
 
 public class AccessibilityPropertySetTest {
+  private AccessibilityPropertySet accessibilityPropertySet;
+
   @Test
   void testDefaultBehaviourOfBuilder() {
-
-    AccessibilityPropertySet expectedValue = new AccessibilityPropertySet.Builder()
+    accessibilityPropertySet = new AccessibilityPropertySet.Builder()
       .withWidth(OptionalDouble.empty())
       .withLit(OptionalBoolean.empty())
       .withSurface(OptionalEnum.empty())
       .build();
-    assertEquals(expectedValue, new AccessibilityPropertySet.Builder().build());
+    assertEquals(accessibilityPropertySet, new AccessibilityPropertySet.Builder().build());
   }
 
   @Test
   void testWidthGetSet() {
-    AccessibilityPropertySet accessibilityPropertySet;
-
     OptionalDouble width = OptionalDouble.of(4.5);
     accessibilityPropertySet = new AccessibilityPropertySet.Builder().withWidth(width).build();
     assertEquals(width, accessibilityPropertySet.getWidth());
@@ -30,10 +30,15 @@ public class AccessibilityPropertySetTest {
 
   @Test
   void testLitGetSet() {
-    AccessibilityPropertySet accessibilityPropertySet;
-
     OptionalBoolean lit = OptionalBoolean.of(true);
     accessibilityPropertySet = new AccessibilityPropertySet.Builder().withLit(lit).build();
     assertEquals(lit, accessibilityPropertySet.getLit());
+  }
+
+  @Test
+  void testSurfaceGetSet() {
+    OptionalEnum surface = OptionalEnum.of(OSMSurface.paved);
+    accessibilityPropertySet = new AccessibilityPropertySet.Builder().withSurface(surface).build();
+    assertEquals(surface, accessibilityPropertySet.getSurface());
   }
 }
