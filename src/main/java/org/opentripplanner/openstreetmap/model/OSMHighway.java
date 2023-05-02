a2712c95dd78a1a2d514da413e884f78cee5f5fc
Xavier <xavier.etchevers@orange.com>
2023-05-02 10:41:57 +0200

    Create OSMHighway enum

---
+++ src/main/java/org/opentripplanner/openstreetmap/model/OSMHighway.java
@@ -0,0 +1,9 @@
+package org.opentripplanner.openstreetmap.model;
+
+public enum OSMHighway {
+  steps,
+  footway,
+  residential,
+  pedestrian,
+  railway
+}
--- src/main/java/org/opentripplanner/openstreetmap/model/OSMSmoothness.java
+++ src/main/java/org/opentripplanner/openstreetmap/model/OSMSmoothness.java
@@ -1,13 +1,10 @@
 package org.opentripplanner.openstreetmap.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 public enum OSMSmoothness {
   very_bad,
   bad,
   intermediate,
   good,
-  excellent;
+  excellent
 
 }
