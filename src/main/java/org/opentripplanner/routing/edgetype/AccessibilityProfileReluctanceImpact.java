64e20377bc97f56b2f1d9835ecfbddeb086ccf27
Xavier <xavier.etchevers@orange.com>
2023-05-15 14:09:41 +0200

    Managing width value in accordance with PAM and UFR accessibility profile

--- src/main/java/org/opentripplanner/routing/edgetype/AccessibilityProfileReluctanceImpact.java
+++ src/main/java/org/opentripplanner/routing/edgetype/AccessibilityProfileReluctanceImpact.java
@@ -10,7 +10,7 @@ import org.opentripplanner.routing.api.request.preference.AccessibilityProfile;
 public class AccessibilityProfileReluctanceImpact {
   private static final Map<AccessibilityProfile, Map<String, Function<Object, Integer>>> impactOnReluctance = new HashMap<>();
 
-  private static final Function<Object, Integer> widthImpactOnReluctance = value -> {
+  private static final Function<Object, Integer> widthImpactForPAM = value -> {
     Integer result = 1;
     if (value instanceof Double valueAsDouble) {
       if (valueAsDouble < 0.8) result = 3;
@@ -21,9 +21,24 @@ public class AccessibilityProfileReluctanceImpact {
     return result;
   };
 
+  private static final Function<Object, Integer> widthImpactForUFR = value -> {
+    Integer result = 1;
+    if (value instanceof Double valueAsDouble) {
+      if (valueAsDouble < 0.8) result = 5;
+      else if (valueAsDouble < 0.9) result = 4;
+      else if (valueAsDouble < 1.2) result = 3;
+      else if (valueAsDouble < 1.4) result = 2;
+      else result = 1;
+    }
+    return result;
+  };
+
   static {
     impactOnReluctance.put(AccessibilityProfile.PAM, new HashMap<>());
-    impactOnReluctance.get(AccessibilityProfile.PAM).put("width", widthImpactOnReluctance);
+    impactOnReluctance.get(AccessibilityProfile.PAM).put("width", widthImpactForPAM);
+
+    impactOnReluctance.put(AccessibilityProfile.UFR, new HashMap<>());
+    impactOnReluctance.get(AccessibilityProfile.UFR).put("width", widthImpactForUFR);
   }
 
   static double computeRegularWalkReluctanceWithAccessibilityProfile(
